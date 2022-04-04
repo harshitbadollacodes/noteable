724ef6bb78699124eac5af1704f87247c992b4e5
harshitbadolla <harshitbadolla@yahoo.com>
2022-04-04 22:05:14 +0530

    removed extra space

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import noteReducer from "../features/note/noteSlice";
 import userReducer from "../features/user/userSlice";
-
-
-
 export const store = configureStore({
     reducer: {
         user: userReducer,
